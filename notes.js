ffb780ccad965374c5c5c1a5277be752b2cf759a
khadyothan <dasarikhadyothan@gmail.com>
2021-07-11 18:58:05 +0530

    Add database

---
+++ notes.js
@@ -0,0 +1,46 @@
+/*
+Setting up the Blog Project
+
+To start with any kind of project we are cloning from the github we need to use
+"npm install"
+to install all the packages that are required in the package.json file
+
+*/
+
+
+/*
+Challenge 6
+
+EJS looks for the views folder to run ejs pages or partials.
+So the adress of the files need to be relative to the views folder.
+
+*/
+
+
+/*
+Challenge 9
+
+1. We use textarea to get multiple lines input.
+2. Then we use bootstrap classes to get the required styling (class="form-control")
+
+*/
+
+
+/*
+Challenge 13
+
+Logic worked when used forEach loop but not for loop.
+
+*/
+
+
+/*
+Challenge 18
+
+1. lodash.com
+  - A utility library that makes it easier to work with javascript inside your node apps.
+2. Install and require the lodash library
+3. Check for lowercase keyword in the lodash documentation
+4. Challenge is to make a url with "".../posts/another-post" to work and console log match found.
+
+*/
